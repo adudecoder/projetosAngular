692b1c101b307c3185a48b03e754ab479a50361c
Victor Martins <vitorchiesa74@gmail.com>
2022-04-23 15:18:52 -0300

    Protegendo Rotas com Guards

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,23 +4,39 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './courses/courses.component';
 import { StarComponent } from './star/star.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavbarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
     TooltipModule.forRoot(),
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule.forRoot([ // espera um array de objetos do tipo rotas
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full' // Rota que corresponde a raiz da aplicação
+      },
+      {
+        path: 'courses', component: CourseComponent // Rota de redirecionamento
+      },
+      {
+        path: '**', component: Error404Component // Rota caso ele não encontre as rotas iniciais
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- course-manager/src/app/courses/courses.component.ts
+++ course-manager/src/app/courses/courses.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from './courses';
 
 @Component({
-  selector: 'app-courses-list',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
